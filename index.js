9803bf52b5e740f4e88505ebde38e2cee7f3e56d
rainbowmanWEB <94617890+rainbowmanWEB@users.noreply.github.com>
2021-12-02 19:55:35 +0300

    sidebad add
    sidibar and logo layout and adaptive

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import './scss/main.scss';
 import './js/main.js';
+import dasboard from './images/svg-icons/dashboard.svg';
+import wallet from './images/svg-icons/wallet.svg';
+import dasbopaperPlaneard from './images/svg-icons/paper-plane.svg';
+import swap from './images/svg-icons/swap.svg';
+import user from './images/svg-icons/user.svg';
